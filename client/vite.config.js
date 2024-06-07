b9a1fe0b23b0ed64789b7644bc740478ea7340ff
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-06-07 21:17:46 +0530

    Deploying 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // Make sure this is the correct output directory
+  },
   server: {
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
