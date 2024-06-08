85df15b28661c8f7498fe93503d6de9a8c0ac608
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-06-08 13:07:42 +0530

    last

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Make sure this is the correct output directory
+    outDir: 'dist' 
   },
   server: {
     port: 5173,
