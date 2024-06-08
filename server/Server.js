967b10e22da6a0ec713daa345c134cd65d978537
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-06-08 17:49:12 +0530

    updare server.js

--- server/Server.js
+++ server/Server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const path = require('path');
+
 const authRoutes = require('./Route/authRoute');
 const contactRoutes = require('./Route/ContactRoute');
 require('dotenv').config();
 
 connectDB();
 
+// Correct declaration of __dirname
 const __dirname = path.resolve();
 
 const app = express();
