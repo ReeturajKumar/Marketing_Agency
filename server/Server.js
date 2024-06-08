cd9b2baf92384de7f45320a8348883223fd6113f
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-06-08 17:43:34 +0530

    updateing server

--- server/Server.js
+++ server/Server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const path = require ('path')
+const path = require('path');
 const authRoutes = require('./Route/authRoute');
 const contactRoutes = require('./Route/ContactRoute');
 require('dotenv').config();
 
 connectDB();
 
-
-const__dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
