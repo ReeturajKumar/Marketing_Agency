ed2d4084a8d2deb093d8a62ad40179b1ac57a80c
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-06-08 13:55:06 +0530

    deploying 2

--- Server.js
+++ Server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/contact', contactRoutes);
 
-app.use(express,static(path.join(__dirname,'./client/dist')));
+app.use(express.static(path.join(__dirname,'./client/dist')));
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
