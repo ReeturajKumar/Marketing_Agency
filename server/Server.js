36c23f4ee94a3e163301c7649e51b0bdd9facc46
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-06-08 18:33:06 +0530

    remove

--- server/Server.js
+++ server/Server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const path = require('path');
 
 const authRoutes = require('./Route/authRoute');
 const contactRoutes = require('./Route/ContactRoute');
@@ -16,11 +15,6 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/contact', contactRoutes);
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
