e767a20aa074906c6a512d69177eb49291647eb3
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-06-10 11:07:41 +0530

    Server.js

--- server/Server.js
+++ server/Server.js
@@ -1,20 +1,50 @@
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
-
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose'); 
 const authRoutes = require('./Route/authRoute');
 const contactRoutes = require('./Route/ContactRoute');
 require('dotenv').config();
 
-connectDB();
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/contact', contactRoutes);
 
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
+});
